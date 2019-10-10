d9790d542d9557d9a7df96accaf7e010274b78e7
heracoder <iameneandre@gmail.com>
2019-10-10 02:50:48 +0100

    how it works page updated

--- howItWorks.php
+++ howItWorks.php
@@ -57,73 +57,199 @@
         </div>  
       </nav>
 
-      <div class="col-md-12 about-us-notes">
-        <div class="row">
-          <div class="col-md-12 text-center"><h2>How Does Irion Work?</h2></div>
-        </div>
-     
-        <p>
-          Irion was created with the following mission: "to promote the greatest good,
-          with a particular emphasis on helping man and the environment grow".
-          <b>Our Vision</b> is to make you believe that you have the ability to earn 
-          without having to work a 9:00am to 6:00pm or rely on anyone else or any form of 
-          lottery for money because you can grow your cash here.
-          To provide the system and support needed to enable our members achieve our vision.
-          The wellbeing of the populace are our long-term lead.
-        </p>
-        <p> Irion is here, dedicated at providing people the opportunity to 
-            become empowered at all times. We present you with opportunities on how to take
-            control of your finance growth and adequately qualify you for multiple streams of income.
-            Irion is unique and second to none that benefits its members in acquiring financial
-            empowerment. The range of our services will get you excited and our membership compensation
-            plans will be more than welcoming.
-        </p>
-     
-        <p>
-          Irion was created with the following mission: "to promote the greatest good,
-          with a particular emphasis on helping man and the environment grow".
-          <b>Our Vision</b> is to make you believe that you have the ability to earn 
-          without having to work a 9:00am to 6:00pm or rely on anyone else or any form of 
-          lottery for money because you can grow your cash here.
-          To provide the system and support needed to enable our members achieve our vision.
-          The wellbeing of the populace are our long-term lead.
-        </p>
-        <p> Irion is here, dedicated at providing people the opportunity to 
-            become empowered at all times. We present you with opportunities on how to take
-            control of your finance growth and adequately qualify you for multiple streams of income.
-            Irion is unique and second to none that benefits its members in acquiring financial
-            empowerment. The range of our services will get you excited and our membership compensation
-            plans will be more than welcoming.
-        </p>
-     
-        <p>
-          Irion was created with the following mission: "to promote the greatest good,
-          with a particular emphasis on helping man and the environment grow".
-          <b>Our Vision</b> is to make you believe that you have the ability to earn 
-          without having to work a 9:00am to 6:00pm or rely on anyone else or any form of 
-          lottery for money because you can grow your cash here.
-          To provide the system and support needed to enable our members achieve our vision.
-          The wellbeing of the populace are our long-term lead.
-        </p>
-        <p> Irion is here, dedicated at providing people the opportunity to 
-            become empowered at all times. We present you with opportunities on how to take
-            control of your finance growth and adequately qualify you for multiple streams of income.
-            Irion is unique and second to none that benefits its members in acquiring financial
-            empowerment. The range of our services will get you excited and our membership compensation
-            plans will be more than welcoming.
-        </p>
-        <div class="row">
-          <h5 class="col-md-12 text-center">How does Irion Work?</h5>
-        </div>
-        <div class="col-md-12">
-          <p class="text-center"> Irion is a simple collective earning system in which members
-            earn by referals.This system is made in such a way where people can earn as much as 
-            they want for as little work as referals 
-          </p>
-        </div>
+      <div class="col-md-12 works-title">
+        <h2 class="text-center">How Does Irion Work?</h2>
+    
+        <!-- LEVEL ZERO HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 0</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>5,000 Upgrade/Reg Fee</h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>0 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>0 Gained</h6></div>
       </div>
+      <!-- LEVEL ZERO BODY  -->
+      <div class="row">
+          <div class="col-md-12 pl-5">
+              <ol>
+                <li><span class="a">-</span> Welcome to Irion.</li>
+                <li><span class="b">-</span> This is the registration money.</li>
+                <li><span class="c">-</span> You pay 5k to your upliner to activate level 1.</li>
+                <li><span class="d">-</span>  You gain the right to earn.</li>
+              </ol>
+          </div>
+      </div>
+
+        <!-- LEVEL ONE HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 1</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>10,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>20,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>10,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL ONE BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Four users will be given to you as your downlines.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 10k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 5k which amounts to the 20k.</li>
+              <li><span class="d">-</span> After paying 10k to your upliner, you gain 10k.</li>
+            </ol>
+        </div>
     </div>
 
+    <!-- LEVEL TWO HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 2</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>15,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>40,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>25,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL TWO BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 2.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 15k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 10k which amounts to the 40k.</li>
+              <li><span class="d">-</span> After paying 15k to your upliner, you gain 25k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL THREE HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 3</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>20,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>60,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>40,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL THREE BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 3   .</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 20k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 15k which amounts to the 60k.</li>
+              <li><span class="d">-</span> After paying 20k to your upliner, you gain 40k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL FOUR HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 4</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>30,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>80,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>50,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL FOUR BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 4.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 30k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 20k which amounts to the 80k.</li>
+              <li><span class="d">-</span> After paying 30k to your upliner, you gain 50k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL FIVE HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 5</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>40,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>120,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>80,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL FIVE BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 5.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 40k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 30k which amounts to the 120k.</li>
+              <li><span class="d">-</span> After paying 40k to your upliner, you gain 80k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL SIX HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 6</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>50,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>160,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>110,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL SIX BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 6.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 50k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 40k which amounts to the 160k.</li>
+              <li><span class="d">-</span> After paying 50k to your upliner, you gain 110k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL SEVEN HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 7</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>60,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>200,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>140,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL SEVEN BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 7.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 60k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 50k which amounts to the 200k.</li>
+              <li><span class="d">-</span> After paying 60k to your upliner, you gain 140k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL EIGHT HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 8</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>70,000 Upgrade Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>240,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>170,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL SEVEN BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 8.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 70k to your upliner to activate next level.</li>
+              <li><span class="c">-</span> Each user under you pays 60k which amounts to the 240k.</li>
+              <li><span class="d">-</span> After paying 70k to your upliner, you gain 170k.</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- LEVEL NINE HEADERS  -->
+      <div class="row pt-4">
+        <div class="col-md-2 text-center a"><h5>Level 9</h5></div>
+        <div class="col-md-3 text-center b"><h6><span>&#8358;</span>70,000 Upgrade/Renewal Fee  </h6></div>
+        <div class="col-md-3 text-center c"><h6><span>&#8358;</span>280,000 Recieved</h6></div>
+        <div class="col-md-3 text-center d"><h6><span>&#8358;</span>210,000 Gained</h6></div>
+      </div>
+          <!-- LEVEL NINE BODY  -->
+      <div class="row">
+        <div class="col-md-12 pl-5">
+            <ol>
+              <li><span class="a">-</span> Welcome to level 9.</li>
+              <li><span class="b">-</span> After all your downlines have paid you, you pay 70k to your upliner to RENEW your account.</li>
+              <li><span class="c">-</span> Each user under you pays 70k which amounts to the 280k.</li>
+              <li><span class="d">-</span> After paying 70k to your upliner, you gain 170k.</li>
+              <li><span class="d">-</span> After renewal, you are to restart the cycle to earn again as you did till this level.</li>
+            </ol>
+        </div>
+    </div>
     <!-- footer  -->
     <div class="row footer">
      <div class="col-md-12 text-center m-1">
---
+++ userWorks.php
@@ -0,0 +1,226 @@
+<?php
+  include 'userDashHeader.php';
+  // sidebar page
+include 'userDashSideNav.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description"
+        content="This content provides you the ability to grow your money safely online by referal">
+    <meta name="keywords" content="online,money,finance,growth">
+    <meta name="author" content="Irion global">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="css/bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+    <body>
+        <div class="dashboard-wrappers">
+            <div class="col-md-12 works-title">
+                <h2 class="text-center">How Does Irion Work?</h2>
+            
+                <!-- LEVEL ZERO HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 0</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>5,000 Upgrade/Reg Fee</h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>0 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>0 Gained</h6></div>
+            </div>
+            <!-- LEVEL ZERO BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                        <li><span class="a">-</span> Welcome to Irion.</li>
+                        <li><span class="b">-</span> This is the registration money.</li>
+                        <li><span class="c">-</span> You pay 5k to your upliner to activate level 1.</li>
+                        <li><span class="d">-</span>  You gain the right to earn.</li>
+                    </ol>
+                </div>
+            </div>
+
+                <!-- LEVEL ONE HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 1</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>10,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>20,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>10,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL ONE BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Four users will be given to you as your downlines.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 10k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 5k which amounts to the 20k.</li>
+                    <li><span class="d">-</span> After paying 10k to your upliner, you gain 10k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL TWO HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 2</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>15,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>40,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>25,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL TWO BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 2.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 15k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 10k which amounts to the 40k.</li>
+                    <li><span class="d">-</span> After paying 15k to your upliner, you gain 25k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL THREE HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 3</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>20,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>60,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>40,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL THREE BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 3   .</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 20k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 15k which amounts to the 60k.</li>
+                    <li><span class="d">-</span> After paying 20k to your upliner, you gain 40k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL FOUR HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 4</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>30,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>80,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>50,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL FOUR BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 4.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 30k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 20k which amounts to the 80k.</li>
+                    <li><span class="d">-</span> After paying 30k to your upliner, you gain 50k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL FIVE HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 5</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>40,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>120,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>80,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL FIVE BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 5.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 40k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 30k which amounts to the 120k.</li>
+                    <li><span class="d">-</span> After paying 40k to your upliner, you gain 80k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL SIX HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 6</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>50,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>160,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>110,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL SIX BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 6.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 50k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 40k which amounts to the 160k.</li>
+                    <li><span class="d">-</span> After paying 50k to your upliner, you gain 110k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL SEVEN HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 7</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>60,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>200,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>140,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL SEVEN BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 7.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 60k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 50k which amounts to the 200k.</li>
+                    <li><span class="d">-</span> After paying 60k to your upliner, you gain 140k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL EIGHT HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 8</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>70,000 Upgrade Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>240,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>170,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL SEVEN BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 8.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 70k to your upliner to activate next level.</li>
+                    <li><span class="c">-</span> Each user under you pays 60k which amounts to the 240k.</li>
+                    <li><span class="d">-</span> After paying 70k to your upliner, you gain 170k.</li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- LEVEL NINE HEADERS  -->
+            <div class="row pt-4">
+                <div class="col-md-2 text-center a"><h5>Level 9</h5></div>
+                <div class="col-md-3 text-center b"><h6><span>&#8358;</span>70,000 Upgrade/Renewal Fee  </h6></div>
+                <div class="col-md-3 text-center c"><h6><span>&#8358;</span>280,000 Recieved</h6></div>
+                <div class="col-md-3 text-center d"><h6><span>&#8358;</span>210,000 Gained</h6></div>
+            </div>
+                <!-- LEVEL NINE BODY  -->
+            <div class="row">
+                <div class="col-md-12 pl-5">
+                    <ol>
+                    <li><span class="a">-</span> Welcome to level 9.</li>
+                    <li><span class="b">-</span> After all your downlines have paid you, you pay 70k to your upliner to RENEW your account.</li>
+                    <li><span class="c">-</span> Each user under you pays 70k which amounts to the 280k.</li>
+                    <li><span class="d">-</span> After paying 70k to your upliner, you gain 170k.</li>
+                    <li><span class="d">-</span> After renewal, you are to restart the cycle to earn again as you did till this level.</li>
+                    </ol>
+                </div>
+            </div>
+            
+        </div>
+    </body>
+</html>
