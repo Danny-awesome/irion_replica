d65975a4cb331f4f961dce4a960c3418f18dd0c8
Danny-awesome <dmmachukwu@gmail.com>
2019-10-20 01:22:35 +0100

    update

--- scripts/update_profile.php
+++ scripts/update_profile.php
@@ -3,21 +3,13 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-
 require_once 'config/dbConnect.php';
 
-$errors = array();
-$lastname = "";
-$firstname = "";
-$username = "";
-$email = "";
-$phone = "";
-$pword = "";
-$npword = "";
-$cnpword = "";
-$acctnum = "";
-$acctname = "";
-$bank = "";
+$lastname = $firstname = $username = $email = $phone = $pword = $npword = $cnpword = $acctnum = $acctname = $bank = "";
+
+//error variables
+$numero_uno = $numero_dos = $numero_tres = $numero_cuatro = $numero_cinco = $numero_seis = "";
+$numero_siete = $numero_ocho = $numero_nueve = $numero_diez= "";
 
 if (isset($_POST['update_profile_btn'])) {
     $lastname = $_POST['lastname'];
@@ -31,5 +23,26 @@ if (isset($_POST['update_profile_btn'])) {
     $acctname = $_POST['acctname'];
     $acctnum = $_POST['acctnum'];
     $bank = $_POST['bank'];
+    profileUpdate($lastname, $firstname, $username, $email, $phone, $pword, $npword, $cnpword, $acctname, $acctnum, $bank);
 }
+
+function profileUpdate($lastname, $firstname, $username, $email, $phone, $pword, $npword, $cnpword, $acctname, $acctnum, $bank)
+{
+    global $numero_uno;
+    global $numero_dos;
+    global $numero_tres;
+    global $numero_cuatro;
+    global $numero_cinco;
+    global $numero_seis;
+    global $numero_siete;
+    global $numero_ocho;
+    global $numero_nueve;
+    global $numero_diez;
+
+    //check if fields are empty
+    //check if fields are empty
+
+
+}
+
 ?>
