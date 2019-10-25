986dd88a93f520656f0253d5b99f30ec7fe153c3
heracoder <iameneandre@gmail.com>
2019-10-25 14:20:52 +0100

    update

--- signup.php
+++ signup.php
@@ -10,13 +10,13 @@
     <meta name="description" content="This content provides you the ability to grow your money safely online by referal">
     <meta name="keywords"  content="online,money,finance,growth">
     <meta name="author" content="Irion global"> 
-       <!-- BOOTSTRAP MIN CSS  -->
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- BOOTSTRAP MIN CSS  -->
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <!-- JQUERY 3.4.1  -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="js/jquery-3.4.1.min.js"></script>
     <!-- BOOTSTRAP MIN JS  -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- LOGO  -->
+    <script src="css/bootstrap/js/bootstrap.min.js"></script>
+ <!-- LOGO  -->
   
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/style.css">
